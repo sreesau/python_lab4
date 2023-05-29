d42683466a689a60b4b5fa04c54b7b3226284efb
sreesau <sreesau6@gmail.com>
2023-05-30 01:20:27 +0530

    program to get days between two dates

---
+++ Answer#5.py
@@ -0,0 +1,6 @@
+#Write a Python program to get days between two dates. Go to the editor 
+import datetime
+first_date= datetime.date(2022,1,20)
+second_date= datetime.date(2023,1,20)
+days=second_date-first_date
+print("Days Between two given dates are:",days)
